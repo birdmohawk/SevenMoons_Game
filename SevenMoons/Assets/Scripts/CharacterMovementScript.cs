21efd7f9226d4406845b8d4f13ddfbe5651f1875
bellsye <bellsyeillman@gmail.com>
2021-03-25 23:24:03 +0800

    New Movement Script
    New movement script added - doesn't move diagonally. Added to another character sprite to compare with current movement script

--- SevenMoons/Assets/Scripts/CharacterMovementScript.cs
+++ SevenMoons/Assets/Scripts/CharacterMovementScript.cs
@@ -15,8 +15,9 @@ public class CharacterMovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float moveH = Input.GetAxis("Horizontal"); //left + right arrows
-        float moveV = Input.GetAxis("Vertical"); //up + down arrows
+
+        float moveH = Input.GetAxis("Horizontal"); 
+        float moveV = Input.GetAxis("Vertical"); 
 
         Vector2 moveDirection = new Vector2(moveH, moveV); 
         moveDirection = Vector2.ClampMagnitude(moveDirection, 1); //clamps all directions (diagonals) to same speed
---
+++ SevenMoons/Assets/Scripts/CharacterMovementXYOnly.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovementXYOnly : MonoBehaviour
+{
+    public float speed = 10;
+    Vector2 lastMove;
+    Vector2 moveDirection;
+    bool wasMovingV;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        bool isMovingH = Mathf.Abs(h) > 0f;
+
+        float v = Input.GetAxis("Vertical");
+        bool isMovingV = Mathf.Abs(v) > 0f;
+
+        if (isMovingH && isMovingV) //need to set lastdirection for if 2 keys pressed
+        {
+            if (wasMovingV)
+            {
+                moveDirection = new Vector2(h, 0);
+                transform.Translate(moveDirection * speed * Time.deltaTime);
+                lastMove = new Vector2(h, 0f);
+            }
+
+            else 
+            {
+                moveDirection = new Vector2(0, v);
+                transform.Translate(moveDirection * speed * Time.deltaTime);
+                lastMove = new Vector2(0f, v);
+            }
+        }
+
+        else if (isMovingH)
+        {
+            moveDirection = new Vector2(h, 0);
+            transform.Translate(moveDirection * speed * Time.deltaTime);
+            wasMovingV = false;
+            lastMove = new Vector2(h, 0f);
+        }
+
+        else if (isMovingV)
+        {
+            moveDirection = new Vector2(0, v);
+            transform.Translate(moveDirection * speed * Time.deltaTime);
+            wasMovingV = true;
+            lastMove = new Vector2(h, 0f);
+        }
+
+    }
+}
