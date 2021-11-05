fbdc693e94e80b37fbafb52f7448957ff44215bf
bellsye <bellsyeillman@gmail.com>
2021-11-05 22:31:32 +0800

    Extra push

--- SevenMoons/Assets/Scripts/Sound/RTPCListener.cs
+++ SevenMoons/Assets/Scripts/Sound/RTPCListener.cs
@@ -8,10 +8,10 @@ public class RTPCListener : MonoBehaviour
 
     public AK.Wwise.RTPC DayNumberRTPC;
 
-    public AK.Wwise.Event DayNumber;
+    //public AK.Wwise.Event DayNumber;
 
     // Update is called once per frame
-    void Start()
+    void Awake()
     {
         TimeOfDayRTPC.SetGlobalValue(GameManagerScript.gamemanager.tasks); //put in start?
         DayNumberRTPC.SetGlobalValue(GameManagerScript.gamemanager.nights); //put in start?
@@ -19,49 +19,49 @@ public class RTPCListener : MonoBehaviour
         if (GameManagerScript.gamemanager.nights == 0)
         {
             AkSoundEngine.SetSwitch("Ambience_DayNumber", "Day1", gameObject);
-            DayNumber.Post(gameObject);
+            //DayNumber.Post(gameObject);
         }
 
         if (GameManagerScript.gamemanager.nights == 1)
         {
             AkSoundEngine.SetSwitch("Ambience_DayNumber", "Day2", gameObject);
-            DayNumber.Post(gameObject);
+            //DayNumber.Post(gameObject);
         }
 
         if (GameManagerScript.gamemanager.nights == 2)
         {
             AkSoundEngine.SetSwitch("Ambience_DayNumber", "Day3", gameObject);
-            DayNumber.Post(gameObject);
+            //DayNumber.Post(gameObject);
         }
 
         if (GameManagerScript.gamemanager.nights == 3)
         {
             AkSoundEngine.SetSwitch("Ambience_DayNumber", "Day4", gameObject);
-            DayNumber.Post(gameObject);
+            //DayNumber.Post(gameObject);
         }
 
         if (GameManagerScript.gamemanager.nights == 4)
         {
             AkSoundEngine.SetSwitch("Ambience_DayNumber", "Day5", gameObject);
-            DayNumber.Post(gameObject);
+            //DayNumber.Post(gameObject);
         }
 
         if (GameManagerScript.gamemanager.nights == 5)
         {
             AkSoundEngine.SetSwitch("Ambience_DayNumber", "Day6", gameObject);
-            DayNumber.Post(gameObject);
+            //DayNumber.Post(gameObject);
         }
 
         if (GameManagerScript.gamemanager.nights >=6)
         {
             AkSoundEngine.SetSwitch("Ambience_DayNumber", "Day7", gameObject);
-            DayNumber.Post(gameObject);
+            //DayNumber.Post(gameObject);
         }
 
     }
 
-    void OnDestroy()
+    /*void OnDestroy()
     {
         DayNumber.Stop(gameObject);
-    }
+    }*/
 }
